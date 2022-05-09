68359391662d4c1325fc0910c31c22df0ce0d85f
Christopher <c.q.ezzard@gmail.com>
2022-05-09 08:52:48 -0500

    Docker files

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import Navbar from './Navbar';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -16,7 +19,9 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+const port = process.env.PORT || 8080;
+root.listen(port, () => console.log('App is listening'));
+ 
+
 reportWebVitals();
